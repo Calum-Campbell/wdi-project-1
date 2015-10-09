1595cdcecd68ae98048fff451673756ddc2b5f2c
Calum-Campbell <campbell.cj@hotmail.co.uk>
2015-10-09 11:46:35 +0100

    Basic 4X4 grid set up, move onto JS

--- js/app.js
+++ js/app.js
@@ -1,8 +1,11 @@
 // *************Stevie Wonderland Game ********************
 // Make basic grid with clickable elements
 // Logic for 2 players and points based on matching elements
+// Randomly filling the grid
 //Have points displaying in browser
 // add the secret tracks
 // Get music clips
 // Look into increasing the grid size
 // 
+
+console.log("working")
