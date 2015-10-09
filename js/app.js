b9231530f3c4773ccee6b6d59875db6c8bdb38da
Calum-Campbell <campbell.cj@hotmail.co.uk>
2015-10-09 11:24:12 +0100

    Initial boilerplate setup

---
+++ js/app.js
@@ -0,0 +1,8 @@
+// *************Stevie Wonderland Game ********************
+// Make basic grid with clickable elements
+// Logic for 2 players and points based on matching elements
+//Have points displaying in browser
+// add the secret tracks
+// Get music clips
+// Look into increasing the grid size
+// 
