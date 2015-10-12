f0b7a864b3b787cc166fa816b9d6b98f8b035fe2
Calum-Campbell <campbell.cj@hotmail.co.uk>
2015-10-12 22:13:19 +0100

    Replay added in but final replay function not working yet

--- js/app.js
+++ js/app.js
@@ -1,80 +1,75 @@
 
 //NEED TO DO -
 // back to start/replay button
-// Check array as we go
-// Include other difficulties
-// Stop being able to press buttons once computer sequence is coming up
-// fade in/out of images
+// styling
 
+var emojis = [
+{name: "one", value:1, src: "./emojis/1.png"},
+{name: "two", value:2, src:"./emojis/2.png"},
+{name: "three", value:3, src:"./emojis/3.png"},
+{name: "four", value:4, src:"./emojis/4.png"},
+{name: "five", value:5, src:"./emojis/5.png"},
+{name: "six", value:6, src:"./emojis/6.png"},
+{name: "seven", value:7, src:"./emojis/7.png"},
+{name: "eight", value:8, src:"./emojis/8.png"},
+{name: "nine", value:9, src:"./emojis/9.png"},
+{name: "ten", value:10, src:"./emojis/10.png"},
+{name: "eleven", value:11, src:"./emojis/11.png"},
+{name: "twelve", value:12, src:"./emojis/12.png"},
+{name: "thirteen", value:13, src:"./emojis/13.png"},
+{name: "fourteen", value:14, src:"./emojis/14.png"},
+{name: "fifteen", value:15, src:"./emojis/15.png"},
+{name: "sixteen", value:16, src:"./emojis/16.png"},
+{name: "seventeen", value:17, src:"./emojis/17.png"},
+{name: "eighteen", value:18, src:"./emojis/18.png"},
+{name: "nineteen", value:19, src:"./emojis/19.png"},
+{name: "twenty", value:20, src:"./emojis/20.png"},
+{name: "twentyone", value:21, src:"./emojis/21.png"},
+{name: "twentytwo", value:22, src:"./emojis/22.png"},
+{name: "twentythree", value:23, src:"./emojis/23.png"},
+{name: "twentyfour", value:24, src:"./emojis/24.png"},
+{name: "twentyfive", value:25, src:"./emojis/25.png"},
+{name: "twentyfive", value:26, src:"./emojis/26.png"},
+{name: "twentyfive", value:27, src:"./emojis/27.png"},
+{name: "twentyfive", value:28, src:"./emojis/28.png"},
+{name: "twentyfive", value:29, src:"./emojis/29.png"},
+{name: "twentyfive", value:30, src:"./emojis/30.png"},
+{name: "twentyfive", value:31, src:"./emojis/31.png"},
+{name: "twentyfive", value:32, src:"./emojis/32.png"},
+{name: "twentyfive", value:33, src:"./emojis/33.png"},
+{name: "twentyfive", value:34, src:"./emojis/34.png"},
+{name: "twentyfive", value:35, src:"./emojis/35.png"}];
 
 $(function(){
+  gameStart();
+  function gameStart(){
 
-  var emojis = [
-  {name: "one", value:1, src: "./emojis/1.png"},
-  {name: "two", value:2, src:"./emojis/2.png"},
-  {name: "three", value:3, src:"./emojis/3.png"},
-  {name: "four", value:4, src:"./emojis/4.png"},
-  {name: "five", value:5, src:"./emojis/5.png"},
-  {name: "six", value:6, src:"./emojis/6.png"},
-  {name: "seven", value:7, src:"./emojis/7.png"},
-  {name: "eight", value:8, src:"./emojis/8.png"},
-  {name: "nine", value:9, src:"./emojis/9.png"},
-  {name: "ten", value:10, src:"./emojis/10.png"},
-  {name: "eleven", value:11, src:"./emojis/11.png"},
-  {name: "twelve", value:12, src:"./emojis/12.png"},
-  {name: "thirteen", value:13, src:"./emojis/13.png"},
-  {name: "fourteen", value:14, src:"./emojis/14.png"},
-  {name: "fifteen", value:15, src:"./emojis/15.png"},
-  {name: "sixteen", value:16, src:"./emojis/16.png"},
-  {name: "seventeen", value:17, src:"./emojis/17.png"},
-  {name: "eighteen", value:18, src:"./emojis/18.png"},
-  {name: "nineteen", value:19, src:"./emojis/19.png"},
-  {name: "twenty", value:20, src:"./emojis/20.png"},
-  {name: "twentyone", value:21, src:"./emojis/21.png"},
-  {name: "twentytwo", value:22, src:"./emojis/22.png"},
-  {name: "twentythree", value:23, src:"./emojis/23.png"},
-  {name: "twentyfour", value:24, src:"./emojis/24.png"},
-  {name: "twentyfive", value:25, src:"./emojis/25.png"},
-  {name: "twentyfive", value:26, src:"./emojis/26.png"},
-  {name: "twentyfive", value:27, src:"./emojis/27.png"},
-  {name: "twentyfive", value:28, src:"./emojis/28.png"},
-  {name: "twentyfive", value:29, src:"./emojis/29.png"},
-  {name: "twentyfive", value:30, src:"./emojis/30.png"},
-  {name: "twentyfive", value:31, src:"./emojis/31.png"},
-  {name: "twentyfive", value:32, src:"./emojis/32.png"},
-  {name: "twentyfive", value:33, src:"./emojis/33.png"},
-  {name: "twentyfive", value:34, src:"./emojis/34.png"},
-  {name: "twentyfive", value:35, src:"./emojis/35.png"}];
+    var playerArray = []; 
+    var gameArray   = []; 
+    var roundNumber = 1;   
+    var score       = 0;
+    var gameArrayObjects = [];       
+    var finished = true;    
 
 
-  var playerArray = []; 
-  var gameArray = []; 
-  var roundNumber = 1;   
-  var score = 0;
-  var gameArrayObjects = [];           
-
-
-  $('ol.keyboard').on("click", userChoice)
-  $('li#start-button').on('click',generatePlayerArray)
-  $('li#rules-button').on('click', displayRules)
+    $('ol.keyboard').on("click", userChoice);
+    $('#start-button').on('click', generatePlayerArray);
+    $('#rules-button').on('click', displayRules);
 
  // populate the keyboard with the emoji array
  $.each(emojis, populateKeyboard);
 
- function populateKeyboard(i, emojis){
-  $('ol.keyboard').prepend("<li class='emojis-icon' src="+emojis.src+" style='background-image:url("+emojis.src+");width:45px;height:45px;' id="+ emojis.value +">"+emojis.value+"</li>");
+ function populateKeyboard(i, emoji){
+  $('ol.keyboard').prepend("<li class='emojis-icon' src="+emoji.src+" style='background-image:url("+emoji.src+");width:45px;height:45px;' id="+ emoji.value +">"+emoji.value+"</li>").hide().fadeIn();
 }
 
 function displayRules(){
-  $('.game-screen').html("1. Press play to begin <br> 2. Watch the order of the emojis and repeat them after <br>3. Get the new high score!")
-
+  $('.rules').slideToggle(500);
 }
 
 function generatePlayerArray(){
   clearScreen();
-  $('li#start-button').toggle();
-  $('li#rules-button').toggle();
-  $('li#difficulty-button').toggle();
+  $('#start-button').toggle();
   $('li.blank-button').toggle();
 
   var nextEmoji = emojis[Math.floor(Math.random()*emojis.length)];
@@ -85,17 +80,17 @@ function generatePlayerArray(){
 
 //populate the screeen with a random emoji object.
 function populateScreen(){  
+ finished = false;
+
  var counter = -1;
 
  var timer = setInterval(function(element) {
   counter ++;
   if(counter < roundNumber){
-    console.log("the game array is : "+gameArray)
 
-    $('.game-screen').css("background-image", "url("+gameArrayObjects[counter].src)+")";
-
-    console.log(gameArray);
+    $('.game-screen').css("background-image", "url("+gameArrayObjects[counter].src).hide().fadeIn()+")";
   }else{
+    finished = true;
     clearScreenToGuess();
     clearInterval(timer);
   }
@@ -105,28 +100,25 @@ function populateScreen(){
 
 function clearScreenToGuess(){
   $('.game-screen').css("background-image", "none");
-  $('.game-screen').html("Now You Guess!");
+  $('.game-screen').html("Now You Guess!").hide().fadeIn();
 }
 
 function clearScreen(){
   $('.game-screen').css("background-image", "none");
   $('.game-screen').html("");
+  finished = true;
 }
 
 function gameOverScreen(){
-  $('li#start-button').toggle();
-  $('li#rules-button').toggle();
-  $('li#difficulty-button').toggle();
-  $('li.blank-button').toggle();
-  $('.game-screen').css("background-image", "none");
-  $('.game-screen').html("Game Over, your score is: " + score+ "!");
 
+
+  $('.game-screen').html( "<ol class='game-over'>"+"<li id='replay-button'>Replay</li>"+"<liid='score'>Score: "+score+"</li></ol>");
   score = 0;
 }
 
 function matchScreen(){
   $('.game-screen').css("background-image", "none");
-  $('.game-screen').html("Thats correct! Current Level: " + score+ "!");
+  $('.game-screen').html("Thats correct!").hide().fadeIn();
 
   setTimeout(clearMatchScreen, 800)
   function clearMatchScreen() {     
@@ -138,6 +130,9 @@ function matchScreen(){
 //make array of user choices
 function userChoice(element){
   event.preventDefault();
+  if(!finished) return false;
+  finished = false;
+
   var picLink = (emojis[(element.toElement.id)-1].src)
   var pic = emojis[(element.toElement.id)-1];
 
@@ -148,14 +143,12 @@ function userChoice(element){
 
     $('.game-screen').css("background-image", "url("+picLink+")");
 
-    $('ul.player-selection-bar').prepend("<li class='emojis-icon' src="+pic.src+" style='background-image:url("+pic.src+");width:45px;height:45px;' id="+ pic.value +">"+pic.value+"</li>");
-
-
-  }else{
+    $('ol.player-selection-bar').prepend("<li class='emojis-icon' src="+pic.src+" style='background-image:url("+pic.src+");width:45px;height:45px;' id="+ pic.value +">"+pic.value+"</li>");
+  } else {
     clearScreen();
     $('.game-screen').css("background-image", "url("+picLink+")");
 
-    $('ul.player-selection-bar').prepend("<li class='emojis-icon' src="+pic.src+" style='background-image:url("+pic.src+");width:45px;height:45px;' id="+ pic.value +">"+pic.value+"</li>");
+    $('ol.player-selection-bar').prepend("<li class='emojis-icon' src="+pic.src+" style='background-image:url("+pic.src+");width:45px;height:45px;' id="+ pic.value +">"+pic.value+"</li>");
     setTimeout(clearAndCheckMatch, 800)
     function clearAndCheckMatch() {     
       clearScreen();
@@ -165,10 +158,12 @@ function userChoice(element){
 };
 
 function clearPlayerSelection(){
- $('ul.player-selection-bar').empty();
+ $('ol.player-selection-bar').empty();
 }
 
 function checkMatch(){
+  finshed = true;
+
   var playerArrayString = playerArray.toString();
   var gameArrayString = gameArray.toString();
 
@@ -177,21 +172,23 @@ function checkMatch(){
       score ++;
       roundNumber++;
       playerArray=[];
-      console.log("This is a match");
-      // generatePlayerArray();
       clearPlayerSelection();
       matchScreen();
 
     }else{
-      console.log("not match")
       roundNumber =0;
+      counter = 0;
       playerArray=[];
+      gameArrayString=[];
+      gameArray = [];
       clearPlayerSelection();
       gameOverScreen();
     }
   };
-}
+};
+};
 });
+
 
 
 
