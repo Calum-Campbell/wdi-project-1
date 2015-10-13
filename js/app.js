82ef5912feb65480373bb0a2f258020322a94d0b
Calum-Campbell <campbell.cj@hotmail.co.uk>
2015-10-13 19:34:13 +0100

    Iphone problem fixed

--- js/app.js
+++ js/app.js
@@ -1,43 +1,3 @@
-
-// 7 along top memory thing
-
-var emojis = [
-{name: "one", value:1, src: "./emojis/1.png"},
-{name: "two", value:2, src:"./emojis/2.png"},
-{name: "three", value:3, src:"./emojis/3.png"},
-{name: "four", value:4, src:"./emojis/4.png"},
-{name: "five", value:5, src:"./emojis/5.png"},
-{name: "six", value:6, src:"./emojis/6.png"},
-{name: "seven", value:7, src:"./emojis/7.png"},
-{name: "eight", value:8, src:"./emojis/8.png"},
-{name: "nine", value:9, src:"./emojis/9.png"},
-{name: "ten", value:10, src:"./emojis/10.png"},
-{name: "eleven", value:11, src:"./emojis/11.png"},
-{name: "twelve", value:12, src:"./emojis/12.png"},
-{name: "thirteen", value:13, src:"./emojis/13.png"},
-{name: "fourteen", value:14, src:"./emojis/14.png"},
-{name: "fifteen", value:15, src:"./emojis/15.png"},
-{name: "sixteen", value:16, src:"./emojis/16.png"},
-{name: "seventeen", value:17, src:"./emojis/17.png"},
-{name: "eighteen", value:18, src:"./emojis/18.png"},
-{name: "nineteen", value:19, src:"./emojis/19.png"},
-{name: "twenty", value:20, src:"./emojis/20.png"},
-{name: "twentyone", value:21, src:"./emojis/21.png"},
-{name: "twentytwo", value:22, src:"./emojis/22.png"},
-{name: "twentythree", value:23, src:"./emojis/23.png"},
-{name: "twentyfour", value:24, src:"./emojis/24.png"},
-{name: "twentyfive", value:25, src:"./emojis/25.png"},
-{name: "twentyfive", value:26, src:"./emojis/26.png"},
-{name: "twentyfive", value:27, src:"./emojis/27.png"},
-{name: "twentyfive", value:28, src:"./emojis/28.png"},
-{name: "twentyfive", value:29, src:"./emojis/29.png"},
-{name: "twentyfive", value:30, src:"./emojis/30.png"},
-{name: "twentyfive", value:31, src:"./emojis/31.png"},
-{name: "twentyfive", value:32, src:"./emojis/32.png"},
-{name: "twentyfive", value:33, src:"./emojis/33.png"},
-{name: "twentyfive", value:34, src:"./emojis/34.png"},
-{name: "twentyfive", value:35, src:"./emojis/35.png"}];
-
 var playerArray,
 gameArray,
 roundNumber,
@@ -86,7 +46,6 @@ function generatePlayerArray(){
     var emoji2 = emojis[Math.floor(Math.random()*emojis.length)];
     var emoji3 = emojis[Math.floor(Math.random()*emojis.length)];
 
-   
       gameArrayObjects.push(emoji1);
       gameArrayObjects.push(emoji2);
       gameArrayObjects.push(emoji3);
@@ -110,8 +69,7 @@ function populateScreen(){
   var timer = setInterval(function(element) {
     counter ++;
     if (counter < roundNumber) {
-      console.log(gameArrayObjects[counter])
-      $('.game-screen').css("background-image", "url("+gameArrayObjects[counter].src).hide().fadeIn()+")";
+      $('.game-screen').css("background-image", "url('"+gameArrayObjects[counter].src+"')").hide().fadeIn();
     } else {
       finished = true;
       clearScreenToGuess();
@@ -167,8 +125,6 @@ function userChoice(element){
   var picLink = (emojis[(element.toElement.id)-1].src)
   var pic = emojis[(element.toElement.id)-1];
 
-  console.log(pic)
-
   playerArray.push(parseInt(element.toElement.id));
 
   if (playerArray.length !== gameArray.length){
@@ -177,7 +133,6 @@ function userChoice(element){
     $('.game-screen').css("background-image", "url("+picLink+")");
 
     $('ol.player-selection-bar').prepend("<li class='emojis-icon' src="+pic.src+" style='background-image:url("+pic.src+");width:45px;height:45px;' id="+ pic.value +">"+pic.value+"</li>")
-    console.log(pic)
   } else {
     clearScreen();
     $('.game-screen').css("background-image", "url("+picLink+")");
